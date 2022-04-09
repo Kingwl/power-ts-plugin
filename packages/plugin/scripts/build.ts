0e4bf0e3565933382facd0b50141aa779d4fdf72
kingwl <kingwenlu@gmail.com>
2022-04-09 23:47:04 +0800

    init repo

---
+++ packages/plugin/scripts/build.ts
@@ -0,0 +1,13 @@
+import { build } from 'esbuild'
+import * as path from 'path'
+
+build({
+    entryPoints: [
+        path.resolve(__dirname, '../src/index.ts')
+    ],
+    bundle: true,
+    outdir: path.resolve(__dirname, '../dist'),
+    platform: 'node',
+    format: 'cjs',
+    sourcemap: true
+})
